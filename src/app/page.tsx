4d0f15a1186764dafc8ca553276f36bc49593047
doyourbest96 <creative555.dev@gmail.com>
2024-09-27 12:48:27 -0500

    [update] landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,283 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="landing max-w-7xl px-10 flex flex-col gap-10">
+      <div className="p-7 flex flex-row justify-between items-center gap-10">
+        <div className="flex flex-row items-end gap-28 italic uppercase">
+          <span className="text-3xl font-black">fightclub</span>
+          <div className="flex flex-row items-end gap-10 text-xl font-black text-orange-900">
+            <span>about</span>
+            <span>buy</span>
+            <span>roadmap</span>
+            <span>faq</span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <button className="p-2 text-xl font-extrabold italic uppercase rounded-md bg-orange-950">
+          whitepaper
+        </button>
+      </div>
+      <div className="mt-72 w-1/2 flex flex-col justify-center items-center">
+        <p className="px-28 text-center text-xl font-light">
+          Empowering martial artists, engaging fans and connecting industry
+          leaders for a financially rewarding experience with the FIght Club
+          platform.
+        </p>
+        <button className="my-14 px-4 py-2 flex justify-center items-center gap-4 text-lg font-light rounded-md bg-gradient-to-r from-[#824b3d]/50 from-10% via-[#824b3d] via-50% to-[#824b3d]/50 to-90%">
+          Whitepaper
+        </button>
+      </div>
+      <div className="px-20 py-10 flex flex-row justify-around items-center gap-10 bg-[linear-gradient(to_right,_#000000,_#824b3d,_#824b3d,_#824b3d,_#000000)]">
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold">Empower</p>
+          <p className="font-light">
+            Get control over your career and be seen by millions of people
+            worldwide.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold">Revenue</p>
+          <p className="font-light">
+            Whether you are a fighter, fan or club, our platform offers income
+            models for everyone.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold">Connect</p>
+          <p className="font-light">
+            Build your empire and get in touch with everyone, from fans to
+            industry leaders.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-semibold">Transparency</p>
+          <p className="font-light">
+            By leveraging blockchain nothing can be hidden. Its easy to use and
+            fool proof.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row gap-10">
+        <div className="w-1/2 flex flex-col gap-12">
+          <div className="flex flex-col gap-2">
+            <p>ABOUT FIGHTCLUB</p>
+            <p>
+              {`Fight Club envisions a world where martial arts transcend
+              barriers, empowers individuals and unites communities. To achieve
+              this, we're building a decentralized platform, powered by our
+              native token FICCO, that unlocks unprecedented opportunities for
+              all stakeholders in combat sports—from aspiring amateurs and
+              passionate fans to seasoned professionals and dedicated clubs.
+              Through FICCO, fighters can take control of their careers and
+              engage directly with fans, while fans can support their favorite
+              athletes and invest in their success, fostering a more equitable
+              and rewarding future for the world of combat sports.`}
+            </p>
+            <p>Read the whole story</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="w-1/2">
+              Exclusive FICCO Token SaleS with Guaranteed AND MYSTERY ALLOCATION
+            </p>
+            <div className="py-2 flex justify-center items-center">
+              <Image
+                src={"/assets/images/stage.png"}
+                alt="stage"
+                width={577}
+                height={258}
+              />
+            </div>
+            <p>
+              {`Fight Club's native token, FICCO, is set to launch with a total
+              maximum supply of 100 billion tokens. The initial price for the
+              pre sale of 10 billion tokens will be set at a maximum of 0.00010
+              cent per token, offering an accessible entry point for early
+              adopters and investors of the platform. But there's more: FICCO
+              coins that are not sold before the end date has been reached will
+              be distributed pro-rata to all participants. This means you can
+              get more FICCO than you originally bought, potentially bringing
+              the price per FICCO coin down while increasing the quantity you
+              receive. Terms apply.`}
+            </p>
+            <p>More information</p>
+          </div>
+          <div className="flex flex-col gap-10">
+            <p>FUEL THE REVOLUTION</p>
+            <p>
+              <strong>Ownership and Empowerment:</strong> Purchasing FICCO coins
+              grants you a stake in the Fight Club ecosystem. You become an
+              integral part of a movement that empowers martial artists by
+              providing them direct control over their careers and earning
+              potential.
+            </p>
+            <p>
+              <strong>Fueling the Ecosystem:</strong> FICCO coins power every
+              transaction within the platform. By using FICCO, you directly
+              contribute to the growth and sustainability of the Fight Club
+              community, fostering a vibrant space where talent thrives and
+              opportunities flourish.
+            </p>
+            <p>
+              <strong>Access and Rewards:</strong>{" "}
+              {`Owning FICCO unlocks
+              exclusive benefits within the ecosystem. This includes early
+              access to events, voting rights on platform decisions,
+              participation in exclusive experiences and potential rewards based
+              on the platform's success.`}
+            </p>
+            <p>Learn more about $FICCO</p>
+          </div>
+          <div>
+            <Image
+              src={"/assets/images/tokenomics.png"}
+              alt="tokenomics"
+              width={533}
+              height={577}
+            />
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-start items-center flex-col gap-16">
+          <div className="px-8 py-4 flex flex-row text-center rounded-md border text-black bg-[#d3d3c9]">
+            <div className="pr-10 flex flex-col border-r border-gray-500">
+              <p>FICCO</p>
+              <p className="text-sm">Native Coin</p>
+            </div>
+            <div className="pl-10 flex flex-col">
+              <p>1000000000000</p>
+              <p className="text-sm">Max supply</p>
+            </div>
+          </div>
+          <div>
+            <Image
+              src={"/assets/images/form.png"}
+              alt="form"
+              width={477}
+              height={949}
+            />
+          </div>
+          {/* <div className="rounded-md border">
+            <table>
+              <thead>
+                <tr>
+                  <th>Structure</th>
+                  <th>%</th>
+                  <th>Token Allocation</th>
+                  <th>Vesting schedule</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Pre-sale</td> <td>10%</td> <td>10.000.000.000</td>
+                  <td>100% unlocked on TGE</td>
+                </tr>
+                <tr>
+                  <td>Public sale 1</td> <td>15%</td> <td>15.000.000.000</td>
+                  <td>100% unlocked on TGE</td>
+                </tr>
+                <tr>
+                  <td>Public sale 2 </td>
+                  <td>0%</td> <td>10.000.000.000</td>
+                  <td>25% unlocked followed by 25% quarterly unlock</td>
+                </tr>
+                <tr>
+                  <td>Public sale 3</td> <td>5%</td> <td>5.000.000.000</td>
+                  <td>25% unlocked followed by 25% quarterly unlock</td>
+                </tr>
+                <tr>
+                  <td>Development fund</td> <td>20%</td> <td>20.000.000.000</td>
+                  <td>Linear vesting during 48 months</td>
+                </tr>
+                <tr>
+                  <td>Marketing</td> <td>10%</td> <td>10.000.000.000</td>
+                  <td>Linear vesting during 24 months</td>
+                </tr>
+                <tr>
+                  <td>Liquidity</td> <td>5%</td> <td>5.000.000.000</td>
+                  <td>100% unlocked at TGE</td>
+                </tr>
+                <tr>
+                  <td>Community incent.</td> <td>10%</td>{" "}
+                  <td>10.000.000.000</td>
+                  <td>Linear vesting during 48 months</td>
+                </tr>
+                <tr>
+                  <td>Team</td> <td>8%</td> <td>8.000.000.000</td>
+                  <td>6 months cliff followed by 24 months linear vesting</td>
+                </tr>
+                <tr>
+                  <td>Advisors</td> <td>4%</td> <td>4.000.000.000</td>
+                  <td>6 months cliff followed by 12 months linear vesting</td>
+                </tr>
+                <tr>
+                  <td>Reserve</td> <td>3%</td> <td>3.000.000.000</td>
+                  <td>100% unlocked on TGE</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <p>
+            FICCO coins are held secure in multi-signature wallets, requiring
+            multiple approvals for any transaction, ensuring robust protection
+            against unauthorized access and fraud. For more information please
+            download the whitepaper.
+          </p> */}
+          <div>
+            <Image
+              src={"/assets/images/table.png"}
+              alt="table"
+              width={511}
+              height={836}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={"/assets/images/band.png"}
+          alt="band"
+          width={1149}
+          height={185}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={"/assets/images/roadmap.png"}
+          alt="roadmap"
+          width={1140}
+          height={658}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={"/assets/images/howtobuy.png"}
+          alt="howtobuy"
+          width={1148}
+          height={468}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={"/assets/images/founders.png"}
+          alt="founders"
+          width={1131}
+          height={752}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src={"/assets/images/faq.png"}
+          alt="faq"
+          width={479}
+          height={283}
+        />
+      </div>
+      <div className="mb-10 flex justify-center">
+        <Image
+          src={"/assets/images/footer.png"}
+          alt="footer"
+          width={1147}
+          height={127}
+        />
+      </div>
     </div>
   );
 }
