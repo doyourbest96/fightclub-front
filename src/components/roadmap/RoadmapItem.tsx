374393835a44c5aff4af1e9d50ea4cab545e2073
BTC415 <alenschmitt415@gmail.com>
2024-10-01 21:15:47 +0100

    [refactor] Fixed roadmap issue for pixel perfect

--- src/components/roadmap/RoadmapItem.tsx
+++ src/components/roadmap/RoadmapItem.tsx
@@ -4,6 +4,7 @@ interface RoadmapItemProps {
   title: string;
   contents: string[];
   up: boolean;
+  done: number;
 }
 
 const RoadmapItem = ({ item }: { item: RoadmapItemProps }) => {
@@ -11,9 +12,8 @@ const RoadmapItem = ({ item }: { item: RoadmapItemProps }) => {
     <div className="flex flex-col">
       {item.up && (
         <div
-          className={`mx-4 px-4 flex flex-row gap-4 text-base text-[#dbdbcf] font-bold ${
-            item.up ? "rounded-t-md" : "rounded-b-md"
-          } bg-[#814b3d]`}
+          className={`mx-4 px-4 flex flex-row gap-4 text-base font-roboto-bold text-[#dbdbcf] font-bold ${item.up ? "rounded-t-md" : "rounded-b-md"
+            } bg-[#814b3d]`}
         >
           <span className="py-3">{item.index}</span>
           <span className="w-px h-full border border-black -skew-x-[20deg]"></span>
@@ -24,14 +24,13 @@ const RoadmapItem = ({ item }: { item: RoadmapItemProps }) => {
       )}
       <ul className="p-4 flex flex-col gap-2 rounded-md text-[#dbdbcf] bg-[#151312]">
         {item.contents.map((content, idx) => (
-          <li key={idx}>{content}</li>
+          <li key={idx}>{idx < item.done ? <span className="text-[#7f4236]">{content}</span> : <span>{content}</span>}</li>
         ))}
       </ul>
       {!item.up && (
         <div
-          className={`mx-4 px-4 flex flex-row gap-4 text-base text-[#dbdbcf] font-bold ${
-            item.up ? "rounded-t-md" : "rounded-b-md"
-          } bg-[#814b3d]`}
+          className={`mx-4 px-4 flex flex-row gap-4 text-base font-roboto-bold text-[#dbdbcf] font-bold ${item.up ? "rounded-t-md" : "rounded-b-md"
+            } bg-[#814b3d]`}
         >
           <span className="py-3">{item.index}</span>
           <span className="w-px h-full border border-black -skew-x-[20deg]"></span>
