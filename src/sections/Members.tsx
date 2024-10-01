684c00b99f9d43c2065bb5c68d14ac47c55d26d6
doyourbest <creative555.dev@gmail.com>
2024-10-01 10:47:16 -0500

    [refactor] update members title

--- src/sections/Members.tsx
+++ src/sections/Members.tsx
@@ -6,11 +6,11 @@ const Members = () => {
   return (
     <div id="founders" className="flex flex-col gap-6 rounded-md">
       <div className="flex flex-row items-center gap-4 text-lg font-revoluti text-[#dbdbcf]">
-        <span className="relative">
+        <p className="relative flex flex-row items-center gap-2">
           Founders <span className="w-1.5 h-4 border" /> Advisors
           <span className="w-1.5 h-4 border" /> Ambassadors
           <TrackImg className="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2 -z-10  w-[240px] h-[240px]" />
-        </span>
+        </p>
       </div>
       <div className="hidden lg:block">
         <DesktopMembers />
