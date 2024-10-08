709d8d36a898ebeaec835b9943a50b1957863152
doyourbest <creative555.dev@gmail.com>
2024-10-08 14:44:57 -0500

    [refactor] update font

--- src/components/RoadToGlory/GloryItem.tsx
+++ src/components/RoadToGlory/GloryItem.tsx
@@ -2,14 +2,14 @@ import { GloryItemProps } from "@/types";
 
 const GloryItem = ({ item }: { item: GloryItemProps }) => {
   return (
-    <div className="max-w-[468px] max-h-[456px] min-h-96 w-full p-2 bg-[rgb(3,3,3)] text-[#dbdbcf] rounded-md border border-[#dbdbcf] leading-tight text-sm">
+    <div className="max-w-[468px] max-h-[456px] min-h-96 w-full p-2 bg-[rgb(3,3,3)] text-[#dbdbcf] rounded-md border border-[#dbdbcf] leading-tight">
       <p className="font-roboto-bold text-lg">{item.title}</p>
-      <p className="font-helvetica font-thin">{item.comment}</p>
-      <p className="py-4 font-roboto-thin leading-none text-base">{item.description}</p>
+      <p className="font-roboto-thin">{item.comment}</p>
+      <p className="py-4 font-helvetica leading-none">{item.description}</p>
       {item.features.map((feature, idx) => (
         <p key={idx}>
-          <span className="font-roboto-bold text-base">{feature.title}</span>
-          <span className="font-roboto-thin">{" " + feature.desc}</span>
+          <span className="font-roboto-bold">{feature.title}</span>
+          <span className="font-helvetica">{" " + feature.desc}</span>
         </p>
       ))}
     </div>
