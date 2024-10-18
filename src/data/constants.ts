a0a5107ecf5afc82babd2ed727b9922aab57db73
doyourbest <creative555.dev@gmail.com>
2024-10-17 23:52:05 -0500

    [feat] add constants

---
+++ src/data/constants.ts
@@ -0,0 +1,14 @@
+export const defaultRPC = {
+  mainnet: "https://eth-pokt.nodies.app",
+  sepolia: "https://ethereum-sepolia-rpc.publicnode.com",
+};
+
+export const FiccoTokenContractAddress = {
+  mainnet: "",
+  sepolia: "0x35Fcb8A48469288f5F66e0C7e880506b2a81C730",
+};
+
+export const PresaleContractAddress = {
+  mainnet: "",
+  sepolia: "0x55be0833a2757C2Ea3FB7D8e6a7b8A180B9Ac62a",
+};
