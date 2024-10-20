111958dfd28685ce821ffcbadab736db28ff52b9
doyourbest <creative555.dev@gmail.com>
2024-10-20 09:26:51 -0500

    [update] update time left component

--- src/components/PreSale/TimeLeft.tsx
+++ src/components/PreSale/TimeLeft.tsx
@@ -1,34 +1,46 @@
-import { TimeDifference } from "@/types";
-
 const TimeLeft = ({
-  stageIndex,
+  preSaleStage,
   timeLeft,
 }: {
-  stageIndex: number;
-  timeLeft: TimeDifference;
+  preSaleStage: number;
+  timeLeft: number;
 }) => {
   return (
     <>
       <div className="mb-6">
         <p className="font-bold font-revoluti text-xl text-[#dbdbcf]">
-          {stageIndex === 0 ? "TIME UNTIL START" : "TIME LEFT"}
+          {preSaleStage === 0
+            ? "TIME UNTIL START"
+            : preSaleStage === 1
+            ? "TIME LEFT"
+            : preSaleStage === 2
+            ? "TIME UNTIL CLAIM"
+            : "TIME TO CLAIM"}
         </p>
       </div>
       <div className="px-10 grid grid-cols-4 gap-4 mb-6">
         <div className="bg-[#212121] border border-orange-900 p-1 rounded">
-          <div className="text-2xl font-revoluti">{timeLeft.days}</div>
+          <div className="text-2xl font-revoluti">
+            {Math.floor(timeLeft / 86400) % 30}
+          </div>
           <div className="text-sm">days</div>
         </div>
         <div className="bg-[#212121] border border-orange-900 p-1 rounded">
-          <div className="text-2xl font-revoluti">{timeLeft.hours}</div>
+          <div className="text-2xl font-revoluti">
+            {Math.floor(timeLeft / 3600) % 24}
+          </div>
           <div className="text-sm">hours</div>
         </div>
         <div className="bg-[#212121] border border-orange-900 p-1 rounded">
-          <div className="text-2xl font-revoluti">{timeLeft.minutes}</div>
+          <div className="text-2xl font-revoluti">
+            {Math.floor(timeLeft / 60) % 60}
+          </div>
           <div className="text-sm">minutes</div>
         </div>
         <div className="bg-[#212121] border border-orange-900 p-1 rounded">
-          <div className="text-2xl font-revoluti">{timeLeft.seconds}</div>
+          <div className="text-2xl font-revoluti">
+            {Math.floor(timeLeft) % 60}
+          </div>
           <div className="text-sm">seconds</div>
         </div>
       </div>
