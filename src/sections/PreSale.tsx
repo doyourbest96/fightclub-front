d1b5e3ba1aaaa0359df0a2423d0437bc3280ebe7
doyourbest <creative555.dev@gmail.com>
2024-10-23 04:56:22 -0500

    [update] update presale component

--- src/sections/PreSale.tsx
+++ src/sections/PreSale.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import { ethers, isAddress } from "ethers";
+import { ToastContainer, toast } from "react-toastify";
 
 import useWeb3 from "@/hooks/useWeb3";
 import { classNames } from "@/utils/classNames";
@@ -13,7 +14,7 @@ import PreSaleProgress from "@/components/PreSale/PreSaleProgress";
 import TimeLeft from "@/components/PreSale/TimeLeft";
 import PhaseDisplay from "@/components/PreSale/PhaseDisplay";
 import PaymentTypeSelector from "@/components/PreSale/PaymentTypeSelector";
-import { ToastContainer, toast } from "react-toastify";
+import PreSaleFooter from "@/components/PreSale/PreSaleFooter";
 
 enum PreSaleStage {
   Ready,
@@ -772,20 +773,7 @@ const PreSale: React.FC = () => {
           )}
         </div>
 
-        <div className="flex flex-col items-center justify-between bg-[#353535] border-t border-orange-900 p-2 rounded-b-lg">
-          <span className="text-xs font-bold">Contract address:</span>
-          <div className="flex items-center gap-2 mt-1 justify-center w-[90%]">
-            <span className="text-xs">TBA</span>
-            {/* <button
-              className="px-2 py-0.5 text-xs rounded-md bg-[#824B3D] hover:focus:bg-orange-800"
-              onClick={() =>
-                copyToClipboard("0x5384545c3190474713bdc48c3371fdbccd2b8e9")
-              }
-            >
-              COPY
-            </button> */}
-          </div>
-        </div>
+        <PreSaleFooter />
         <ToastContainer />
       </div>
     </div>
