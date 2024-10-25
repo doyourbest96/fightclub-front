cf7971378212559b26f2c8b3a3c755c6a3389e62
doyourbest <creative555.dev@gmail.com>
2024-10-25 03:28:31 -0500

    [update] presale footer component

--- src/components/PreSale/PreSaleFooter.tsx
+++ src/components/PreSale/PreSaleFooter.tsx
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 import { FaRegCopy } from "react-icons/fa6";
-import { PresaleContractAddress } from "@/data/constants";
+import { FiccoTokenContractAddress } from "@/data/constants";
 
 const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text);
@@ -12,12 +12,12 @@ const PreSaleFooter = () => {
     <div className="flex flex-col items-center justify-between bg-[#353535] border-t border-orange-900 p-2 rounded-b-lg">
       <span className="text-xs font-bold">Contract address:</span>
       <div className="flex items-center gap-2 mt-1 justify-center w-[90%]">
-        <span className="text-xs">{PresaleContractAddress.sepolia}</span>
+        <span className="text-xs">{FiccoTokenContractAddress.sepolia}</span>
         <button
           className="px-3 py-1 text-xs rounded-md bg-[#824B3D] hover:bg-orange-800"
-          onClick={() => copyToClipboard(PresaleContractAddress.sepolia)}
+          onClick={() => copyToClipboard(FiccoTokenContractAddress.sepolia)}
         >
-         <FaRegCopy className="w-3 h-3"/>
+          <FaRegCopy className="w-3 h-3" />
         </button>
       </div>
     </div>
