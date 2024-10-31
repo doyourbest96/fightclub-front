c147af40cc257f3b62795e6fb4d2e9eaa3bb2bc6
doyourbest <creative555.dev@gmail.com>
2024-10-31 00:33:08 -0500

    [update] update faq component using data

--- src/sections/Faq.tsx
+++ src/sections/Faq.tsx
@@ -1,52 +1,12 @@
 "use client";
+import dynamic from "next/dynamic";
 import { useState } from "react";
 import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
-import dynamic from "next/dynamic";
+import { faqData } from "@/data/faq.data";
 
 const TrackImg = dynamic(() => import("@/components/trackImg"), {
   ssr: false,
 });
-
-interface FAQItem {
-  question: string;
-  answer: string;
-}
-const faqItems: FAQItem[] = [
-  {
-    question: "What is FICCO coin?",
-    answer:
-      "FICCO is the native cryptocurrency of the IRONWILL platform, designed to facilitate transactions within the decentralized ecosystem, including sponsorships, match funding, and access to exclusive content.",
-  },
-  {
-    question: "How do I purchase FICCO coins from the pre-sale?",
-    answer:
-      "You can purchase FICCO coins during the pre-sale by participating in the initial fundraising stage, which allows early investors to acquire tokens before the public sale.",
-  },
-  {
-    question: "Which blockchain network is FICCO coin on?",
-    answer: "FICCO coin is built on the Ethereum blockchain.",
-  },
-  {
-    question: "When can I claim my coins?",
-    answer:
-      "You can claim your coins after the Token Generation Event (TGE), with specific release schedules depending on the sale phase.",
-  },
-  {
-    question: "How can I store and manage my FICCO coins?",
-    answer:
-      "You can store and manage your FICCO coins in a compatible cryptocurrency wallet that supports Ethereum-based tokens.",
-  },
-  {
-    question: "For what can I use my FICCO coins?",
-    answer:
-      "You can use your FICCO coins for various purposes, including funding matches, purchasing exclusive content, merchandise, and participating in community features.",
-  },
-  {
-    question: "Can I sell my FICCO coins?",
-    answer:
-      "Yes, you can sell your FICCO coins on exchanges where they are listed after the public sale.",
-  },
-];
 
 export default function FiccoFAQ() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -65,7 +25,7 @@ export default function FiccoFAQ() {
           </span>
         </h2>
         <div className="font-roboto-bold">
-          {faqItems.map((item, index) => (
+          {faqData.map((item, index) => (
             <div key={index}>
               <button
                 className="flex justify-between items-center w-full py-1 text-left focus:outline-none group"
