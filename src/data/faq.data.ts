1694a35936e1fa3aff3afefe950f4049901598fd
doyourbest <creative555.dev@gmail.com>
2024-10-31 00:32:49 -0500

    [feat] add faq data

---
+++ src/data/faq.data.ts
@@ -0,0 +1,38 @@
+import { FAQItemProps } from "@/types";
+
+export const faqData: FAQItemProps[] = [
+  {
+    question: "What is FICCO coin?",
+    answer:
+      "FICCO is the native cryptocurrency of the IRONWILL platform, designed to facilitate transactions within the decentralized ecosystem, including sponsorships, match funding, and access to exclusive content.",
+  },
+  {
+    question: "How do I purchase FICCO coins from the pre-sale?",
+    answer:
+      "You can purchase FICCO coins during the pre-sale by participating in the initial fundraising stage, which allows early investors to acquire tokens before the public sale.",
+  },
+  {
+    question: "Which blockchain network is FICCO coin on?",
+    answer: "FICCO coin is built on the Ethereum blockchain.",
+  },
+  {
+    question: "When can I claim my coins?",
+    answer:
+      "You can claim your coins after the Token Generation Event (TGE), with specific release schedules depending on the sale phase.",
+  },
+  {
+    question: "How can I store and manage my FICCO coins?",
+    answer:
+      "You can store and manage your FICCO coins in a compatible cryptocurrency wallet that supports Ethereum-based tokens.",
+  },
+  {
+    question: "For what can I use my FICCO coins?",
+    answer:
+      "You can use your FICCO coins for various purposes, including funding matches, purchasing exclusive content, merchandise, and participating in community features.",
+  },
+  {
+    question: "Can I sell my FICCO coins?",
+    answer:
+      "Yes, you can sell your FICCO coins on exchanges where they are listed after the public sale.",
+  },
+];
